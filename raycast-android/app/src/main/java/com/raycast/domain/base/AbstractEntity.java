bf4411648d71a29e72d07a9e13049bf029543722
Lucas Espindola <espdlucas@gmail.com>
2014-08-29 12:30:15 -0300

    feat(android) add MessageService will handle message API calls

--- raycast-android/app/src/main/java/com/raycast/domain/base/AbstractEntity.java
+++ raycast-android/app/src/main/java/com/raycast/domain/base/AbstractEntity.java
@@ -1,8 +1,14 @@
 package com.raycast.domain.base;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * Created by Lucas on 29/08/2014.
  */
+
+//Found no use to have the IP field on this client, server perhaps should NOT return the field as default
+@JsonIgnoreProperties({"ip"})
 public class AbstractEntity {
     private long id;
 
