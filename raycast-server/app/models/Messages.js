1bb4b5e31241371024e15bd5cd81862084df40e0
Leandro <leandropontesrj@hotmail.com>
2014-08-29 22:10:27 -0300

    fix(server) merge correction

--- raycast-server/app/models/Messages.js
+++ raycast-server/app/models/Messages.js
@@ -16,12 +16,20 @@ MessageSchema.static('findByAuthor', function (author, callback) {
 });
 
 MessageSchema.static('findByRadius', function (radius, latitude, longitude, skip, limit, callback) {
-	return this.find(
-		{"loc":{"$geoWithin":{"$centerSphere":[[ Number(longitude) , Number(latitude) ], radius/3959000]}}},
-		null,
-		{sort: {time: -1}, skip: Number(skip), limit: Number(limit)},
-		callback
-	).limit(Number(limit));
+    var lat = Number(latitude) || null;
+    var lon = Number(longitude) || null;
+    var r = (radius / 3959000) || null;
+
+    if((lat == null) || (lon == null) || (r == null)){
+        return null;
+    }
+
+    return this.find(
+        {"loc":{"$geoWithin":{"$centerSphere":[[ lon , lat ], r]}}},
+        null,
+        {sort: {time: -1}, skip: Number(skip), limit: Number(limit)},
+        callback
+    );
 });
 
 module.exports = mongoose.model('Messages', MessageSchema);
