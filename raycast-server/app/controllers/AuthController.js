b83fba76ba1e15fa0ad187b783c0e9261cb7a3b6
Lucas Espindola <espdlucas@gmail.com>
2014-09-28 01:09:27 -0300

    feat(server) include oauth strategies

--- raycast-server/app/controllers/AuthController.js
+++ raycast-server/app/controllers/AuthController.js
@@ -2,9 +2,14 @@
  * Created by lucas on 9/27/14.
  */
 
-var passport = require('passport');
-var BasicStrategy = require('passport-http').BasicStrategy;
-var User = require('../models/User');
+var User                    = require('../models/User');
+var Client                  = require('../models/oauth/Client');
+var AccessToken             = require('../models/oauth/AccessToken');
+var RefreshToken            = require('../models/oauth/RefreshToken');
+var passport                = require('passport');
+var BasicStrategy           = require('passport-http').BasicStrategy;
+var ClientPasswordStrategy  = require('passport-oauth2-client-password').Strategy;
+var BearerStrategy          = require('passport-http-bearer').Strategy;
 
 passport.use(new BasicStrategy(
     function(username, password, callback) {
@@ -23,6 +28,42 @@ passport.use(new BasicStrategy(
 
                 // Success
                 return callback(null, user);
+            });
+        });
+    }
+));
+
+passport.use(new ClientPasswordStrategy(
+    function(clientId, clientSecret, done) {
+        Client.findOne({ clientId: clientId }, function(err, client) {
+            if (err) { return done(err); }
+            if (!client) { return done(null, false); }
+            if (client.clientSecret != clientSecret) { return done(null, false); }
+
+            return done(null, client);
+        });
+    }
+));
+
+passport.use(new BearerStrategy(
+    function(accessToken, done) {
+        AccessToken.findOne({ token: accessToken }, function(err, token) {
+            if (err) { return done(err); }
+            if (!token) { return done(null, false); }
+
+            if( Math.round((Date.now()-token.created)/1000) > config.get('security:tokenLife') ) {
+                AccessToken.remove({ token: accessToken }, function (err) {
+                    if (err) return done(err);
+                });
+                return done(null, false, { message: 'Token expired' });
+            }
+
+            User.findById(token.userId, function(err, user) {
+                if (err) { return done(err); }
+                if (!user) { return done(null, false, { message: 'Unknown user' }); }
+
+                var info = { scope: '*' }
+                done(null, user, info);
             });
         });
     }
