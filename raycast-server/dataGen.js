686f686e9ab449e1757d69d4d087d0a545245e93
Lucas Espindola <espdlucas@gmail.com>
2014-10-04 21:03:17 -0300

    feat(server) include auth datagen for tests

---
+++ raycast-server/dataGen.js
@@ -0,0 +1,54 @@
+/**
+ * Created by lucas on 10/4/14.
+ */
+var mongoose            = require('mongoose');
+var faker               = require('faker');
+var config              = require('./app/config/config');
+var User                = require('./app/models/User');
+var Client              = require('./app/models/Client');
+var AccessToken         = require('./app/models/AccessToken');
+var RefreshToken        = require('./app/models/RefreshToken');
+
+mongoose.connect(config.mongoose.uri);
+
+User.remove({}, function(err) {
+    var user = new User({
+        username: 'lucas',
+        password: 'espindola',
+        email: 'espdlucas@gmail.com'
+    });
+    user.save(function(err, user) {
+        if(err) return console.log(err);
+        else console.log('New user - %s:%s',user.username,user.password);
+    });
+
+    for(var i=0; i<4; i++) {
+        user = new User({
+            username: faker.internet.userName(),
+            password: faker.internet.password(),
+            email: faker.internet.email()
+        });
+        user.save(function(err, user) {
+            if(err) return console.log(err);
+            else console.log('New user - %s:%s',user.username,user.password);
+        });
+    }
+});
+
+Client.remove({}, function(err) {
+    var client = new Client({ name: 'Android client', clientId: 'raycast', clientSecret:'android' });
+    client.save(function(err, client) {
+        if(err) return console.log(err);
+        else console.log('New client - %s:%s',client.clientId,client.clientSecret);
+    });
+});
+AccessToken.remove({}, function (err) {
+    if (err) return console.log(err);
+});
+RefreshToken.remove({}, function (err) {
+    if (err) return console.log(err);
+});
+
+setTimeout(function() {
+    mongoose.disconnect();
+}, 3000);
