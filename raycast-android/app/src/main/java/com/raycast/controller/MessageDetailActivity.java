0b24e6c654afa8bbc285e0730fcb9488971fd743
ThiagoBaltar <thiago.baltar@gmail.com>
2014-10-25 14:22:03 -0200

    feat(android) add Pull-to-Refresh to comment list in MessageDetailActivity

--- raycast-android/app/src/main/java/com/raycast/controller/MessageDetailActivity.java
+++ raycast-android/app/src/main/java/com/raycast/controller/MessageDetailActivity.java
@@ -1,6 +1,8 @@
 package com.raycast.controller;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
+import android.widget.AbsListView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -35,6 +37,7 @@ public class MessageDetailActivity extends RaycastBaseActivity {
     public final static String EXTRA_MESSAGEDETAIL_MESSAGEID = "com.raycast.messagedetail.messageid";
     @Extra(EXTRA_MESSAGEDETAIL_MESSAGEID) String messageId;
     @RestService RaycastRESTClient raycastRESTClient;
+    @ViewById(R.id.swipe_area) SwipeRefreshLayout swipeArea;
     @ViewById(R.id.messagedetail_message) TextView msgText;
     @ViewById(R.id.messagedetail_editcomment) EditText editComment;
     @ViewById(R.id.messagedetail_commentlist) ListView commentList;
@@ -45,6 +48,33 @@ public class MessageDetailActivity extends RaycastBaseActivity {
     @AfterViews
     void afterViews(){
         fetchMessage();
+    }
+
+    @AfterViews
+    void setListeners() {
+        swipeArea.setColorSchemeResources(android.R.color.holo_purple, android.R.color.holo_purple, android.R.color.holo_purple, android.R.color.holo_purple);
+        swipeArea.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                updateCommentUi();
+            }
+        });
+
+        commentList.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem == 0) {
+                    swipeArea.setEnabled(true);
+                } else {
+                    swipeArea.setEnabled(false);
+                }
+            }
+        });
     }
 
     @OptionsItem(R.id.action_refresh)
@@ -68,7 +98,6 @@ public class MessageDetailActivity extends RaycastBaseActivity {
             raycastRESTClient.addComment(messageId, comment);
             comments = raycastRESTClient.getComments(messageId);
             updateCommentUi();
-
         }catch (Exception ex){
             handleException("Não foi possível enviar comentário :(", ex);
         }
@@ -83,9 +112,13 @@ public class MessageDetailActivity extends RaycastBaseActivity {
 
     @UiThread
     void updateCommentUi(){
+        swipeArea.setRefreshing(true);
+
         commentListAdapter.bind(comments);
         commentList.setAdapter(commentListAdapter);
         editComment.setText("");
+
+        swipeArea.setRefreshing(false);
     }
 
     @UiThread
